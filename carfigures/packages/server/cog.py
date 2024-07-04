7e6aabf49c42df9f3e7c9141526cffad3e82d031
HiboMan <142530154+HiboMan@users.noreply.github.com>
2024-07-04 12:36:42 +0000

    Update cog.py

--- carfigures/packages/server/cog.py
+++ carfigures/packages/server/cog.py
@@ -20,6 +20,8 @@ activation_embed = discord.Embed(
     f"- No farming (spamming or creating servers for {settings.collectible_name}s)\n"
     "- Do not attempt to abuse the bot's internals\n"
     "**Not respecting these rules will lead to a blacklist**",
+    f"- Selling or exchanging {settings.collectible_name}s "
+    "against money or other goods is forbidden\n"
 )
 
 
